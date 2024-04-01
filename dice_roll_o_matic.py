dcb47a81ef31b5ce2d1f843b1bfb9550ba06d80f
Jack McMahon <j.mcmahon@outlook.ie>
2024-04-01 17:15:01 +0100

    move startup messages into different file

--- dice_roll_o_matic.py
+++ dice_roll_o_matic.py
@@ -2,31 +2,7 @@ import re
 from random import randint
 from time import sleep
 
-def bleep_bloop(pause = 0.2):
-	# print either Bleep or Bloop
-	sound = ["Bleep","Bloop"][randint(1,2) - 1]
-	print(sound)
-	sleep(pause)
-bleep_bloop()
-bleep_bloop()
-bleep_bloop()
-bleep_bloop()
-
-print("""---\n
-WELCOME TO DICE-ROLL-O-MATIC
-(c) Applied Dice Applications Labs 1971
-""")
-
-bleep_bloop()
-bleep_bloop()
-
-print("""\n---
-To end your session, type 'end'
-To roll 4 d23s type '4d23'
-To roll 1 d40 type '1d40' or '40'
-Shortcuts: 1d20 = 't' 1d100 = 'h' 1d8 = 'e' 1d6 = 's' 1d4 = 'f'
----\n""")
-
+import startup_messages
 
 shortcuts = {"h": "1d100", "t": "1d20", "e": "1d8", "s": "1d6", "f": "1d4"}
 x = ""
